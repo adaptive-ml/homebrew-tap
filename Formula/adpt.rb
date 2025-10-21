719b82d3d820073153f88ee445d82da9f103c440
Keir Lawson <keirlawson@gmail.com>
2025-10-21 16:13:41 +0200

    correct casing

--- Formula/adpt.rb
+++ Formula/adpt.rb
@@ -1,4 +1,4 @@
-class adpt < Formula
+class Adpt < Formula
   homepage "https://github.com/adaptive-ml/adpt"
   url "https://github.com/adaptive-ml/adpt/releases/download/v0.1.0/adpt-macos-aarch64"
   sha256 "eb167f0033caafcc1f613f4edc8f88dd7837e15483e4dc73cb4b35fd792cf1ff"
