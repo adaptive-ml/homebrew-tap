82ebb94c35dd272cd8b133e0ddbe58945b5ce3a6
Keir Lawson <keirlawson@gmail.com>
2025-10-21 16:49:04 +0200

    Add install

--- Formula/adpt.rb
+++ Formula/adpt.rb
@@ -3,6 +3,10 @@ class Adpt < Formula
   url "https://github.com/adaptive-ml/adpt/releases/download/v0.1.1/adpt-macos-aarch64.tar.gz"
   sha256 "6c3f2d62f7fce4346af4b5a59f2b9d1e216a999ca5b1e4d5e77d6776cb25ed40"
 
+  def install
+    bin.install "adpt-macos-aarch64" => "adpt"
+  end
+
   test do
     system bin / "adpt", "--version"
   end
