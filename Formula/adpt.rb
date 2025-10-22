fc9b47b67ea2a9e8e2b16e1e78671010af573855
Keir Lawson <keirlawson@gmail.com>
2025-10-22 10:58:16 +0200

    Use raw binary

--- Formula/adpt.rb
+++ Formula/adpt.rb
@@ -1,6 +1,6 @@
 class Adpt < Formula
   homepage "https://github.com/adaptive-ml/adpt"
-  url "https://github.com/adaptive-ml/adpt/releases/download/v0.1.1/adpt-macos-aarch64.tar.gz"
+  url "https://github.com/adaptive-ml/adpt/releases/download/v0.1.1/adpt-macos-aarch64"
   sha256 "6c3f2d62f7fce4346af4b5a59f2b9d1e216a999ca5b1e4d5e77d6776cb25ed40"
 
   def install
