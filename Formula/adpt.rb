86ec482e47187f5291e02a4888b916b84d75c5b0
Keir Lawson <keirlawson@gmail.com>
2025-10-22 10:59:15 +0200

    update sha

--- Formula/adpt.rb
+++ Formula/adpt.rb
@@ -1,7 +1,7 @@
 class Adpt < Formula
   homepage "https://github.com/adaptive-ml/adpt"
   url "https://github.com/adaptive-ml/adpt/releases/download/v0.1.1/adpt-macos-aarch64"
-  sha256 "6c3f2d62f7fce4346af4b5a59f2b9d1e216a999ca5b1e4d5e77d6776cb25ed40"
+  sha256 "7379a3d41d5533254d4e246fd0e751701f43baacd69f86944ac15a6d469d9d7d"
 
   def install
     bin.install "adpt-macos-aarch64" => "adpt"
