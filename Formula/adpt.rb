7d4e6c29b588918b970ac21fed02dd30c145da4a
Keir Lawson <keirlawson@gmail.com>
2025-10-24 08:02:59 +0100

    adpt 0.3.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/adpt.rb
+++ Formula/adpt.rb
@@ -1,7 +1,7 @@
 class Adpt < Formula
   homepage "https://github.com/adaptive-ml/adpt"
-  url "https://github.com/adaptive-ml/adpt/releases/download/v0.2.0/adpt-macos-aarch64"
-  sha256 "d8f36e93463ebcc6b90a539456693e6c46c9fffbba4cc6d12b49320409e89af9"
+  url "https://github.com/adaptive-ml/adpt/releases/download/v0.3.0/adpt-macos-aarch64"
+  sha256 "1c0adc472d679ce4b8d6d279f13fa43b93c66897e627ac20b3ca0f766cd0bce5"
 
   def install
     bin.install "adpt-macos-aarch64" => "adpt"
